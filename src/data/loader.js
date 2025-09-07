89ad15bfccda8f212f36e63bc2b06018e22bc052
Praveen Swadi <swadi@adobe.com>
2025-09-06 17:47:23 -0700

    Add Raamraksha to data loader
    - Import raamraksha.json in loader.js
    - Add raamraksha to dataMap for dynamic loading
    - Now Raamraksha collection will appear on homepage and be accessible

--- src/data/loader.js
+++ src/data/loader.js
@@ -3,12 +3,14 @@ import atharvashirshaData from './atharvashirsha.json'
 import atharvashirshaPhalashrutiData from './atharvashirsha-phalashruti.json'
 import gayatriData from './gayatri.json'
 import mahamrityunjayaData from './mahamrityunjaya.json'
+import raamrakshaData from './raamraksha.json'
 
 const dataMap = {
   'atharvashirsha': atharvashirshaData,
   'atharvashirsha-phalashruti': atharvashirshaPhalashrutiData,
   'gayatri': gayatriData,
-  'mahamrityunjaya': mahamrityunjayaData
+  'mahamrityunjaya': mahamrityunjayaData,
+  'raamraksha': raamrakshaData
 }
 
 export const loadCollectionData = (collectionId) => {
